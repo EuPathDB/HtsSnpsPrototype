72821996df29970f284870da298ce609c43664c0
Steve Fischer <sfischer@upenn.edu>
2013-08-20 12:49:21 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/HtsSnpsPrototype@57636 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ dumpLocations.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+main(int argc, char *argv[]) {
+	FILE *f1;
+
+  int32_t loc1;
+  int32_t *loc1p = &loc1;
+
+	if ( argc != 2 ) {
+		printf( "usage: %s file1\n", argv[0] );
+		return -1;
+	}
+
+	f1 = fopen(argv[1], "rb");
+	if (f1 == 0) {
+		printf( "Can't open file1 '%s' \n", argv[1] );
+		return -1;
+	}
+	int f1got;
+	f1got = fread(loc1p, 4, 1, f1);
+
+	while(f1got != 0) {
+		printf("%i\n",  loc1);
+		f1got = fread(loc1p, 4, 1, f1);
+	}
+}
+
--- makeLocationFile.pl
+++ makeLocationFile.pl
@@ -3,13 +3,13 @@ my ($outputFile, $seed) = @ARGV;
 die "usage:  makeFakeSnpsFile.pl output_file seed" unless scalar(@ARGV) == 2;
 my $i=0;
 my $bp = 1;
-my $range = 500;
+my $range = 5;
 
 srand $seed;
 my $inc;
 open(my $out, '>:raw', $outputFile) || die "can't open: $!";
 while($i++ < 10) {
-  $inc = int(rand($range));
+  $inc = int(rand($range)) + 1;
   $bp += $inc;
   print $out pack("I", $bp);
 }
