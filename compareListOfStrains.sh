3270890cdc454879afc40e4f1b873f64f97b1fa3
Steve Fischer <sfischer@upenn.edu>
2013-08-20 03:35:37 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/HtsSnpsPrototype@57633 735e2a04-f8fc-0310-8a1b-f2942603c481

--- compareListOfStrains.sh
+++ compareListOfStrains.sh
@@ -1,13 +1,26 @@
 set -e
 date
-./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample1.bin | wc &
-./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample2.bin | wc &
-./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample3.bin | wc &
-./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample4.bin | wc &
-./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample5.bin | wc &
-./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample6.bin | wc &
-./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample7.bin | wc &
-./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample8.bin | wc &
-./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample9.bin | wc &
-./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample10.bin | wc &
+mkfifo f100 f101 f102 f103 f104 f105 f106 f107 f108 f109
+mkfifo f201 f202 f203 f204 f205
+mkfifo f301 f302
+mkfifo f401
+./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample1.bin > f100 &
+./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample2.bin > f101 &
+./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample3.bin > f102 &
+./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample4.bin > f103 &
+./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample5.bin > f104 &
+./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample6.bin > f105 &
+./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample7.bin > f106 &
+./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample8.bin > f107 &
+./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample9.bin > f108 &
+./compareStrainsBinaryC  /eupath/data/htsSnpsPrototype/fakeData/sample11.bin /eupath/data/htsSnpsPrototype/fakeData/sample10.bin > f109 &
+./mergeLocations f100 f101 > f201 &
+./mergeLocations f102 f103 > f202 &
+./mergeLocations f104 f105 > f203 &
+./mergeLocations f106 f107 > f204 &
+./mergeLocations f108 f109 > f205 &
+./mergeLocations f201 f202 > f301 &
+./mergeLocations f203 f204 > f302 &
+./mergeLocations f301 f302 > f401 &
+./mergeLocations f401 f205 > final &
 exit
