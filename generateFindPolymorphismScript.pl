d68efdcfd5976ddcb17fd162029582de769d0d4b
Steve Fischer <sfischer@upenn.edu>
2013-10-01 17:24:06 +0000

    debug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/HtsSnpsPrototype@58451 735e2a04-f8fc-0310-8a1b-f2942603c481

--- generateFindPolymorphismScript.pl
+++ generateFindPolymorphismScript.pl
@@ -1,53 +1,55 @@
 #!/usr/bin/perl
-
+use strict;
 my ($strainsDir, $final, $polymorphismThreshold, $unknownThreshold) = @ARGV;
 
-die "usage: perl generateStrainsCompareScript.pl strains_dir ref_file final_file\n" unless scalar(@ARGV) == 3;
+die "usage: perl generateFindPolymorphismScript.pl strains_dir final_file polymorphism_threshold unknown_threshold\n" unless scalar(@ARGV) == 4;
 
 if ($strainsDir =~ /(.+)\/$/) { $strainsDir = $1 }   # strip trailing /
 
 # get list of files into array
 
-my @mergeQueue;
+my @mergeQueueOriginal;
 
 opendir(my $dh, $strainsDir) || die "can't opendir $strainsDir: $!";
 my $strainsCount = 0;
 foreach my $strainFile (readdir($dh)) {
   if ($strainFile != /^\d+$/) {
-    push(@mergeQueue, $strainFile);
+    push(@mergeQueueOriginal, $strainFile);
     $strainsCount++;
   }
 }
 closedir $dh;
+
+my @mergeQueue = sort {$a <=> $b} @mergeQueueOriginal;
 
 my $fifoCursor = 0;
 
 print "set -e\n";
 print "date\n";
 print "mkfifo ";
-for (my $i = 1; $i <= scalar(@strains) *2; $i++) {
+for (my $i = 1; $i <= scalar(@mergeQueueOriginal); $i++) {
   print "fifo$i ";
 }
 print "\n";
 
 while(1) {
   if (scalar(@mergeQueue) == 1) {
-    my $allMerged = pop(@mergeQueue);
+    my $allMerged = shift(@mergeQueue);
     print "./findPolymorphic $allMerged referenceGenome.dat $strainsCount $polymorphismThreshold $unknownThreshold > $final &\n";
     last;
   } else {
-    my $input1 = pop(@mergeQueue);
-    my $input2 = pop(@mergeQueue);
+    my $input1 = shift(@mergeQueue);
+    my $input2 = shift(@mergeQueue);
     $fifoCursor++;
     push(@mergeQueue, "fifo$fifoCursor");
-    my $strain1 = $input1 =~ /\d+/? $input1 : 0;
-    my $strain2 = $input2 =~ /\d+/? $input2 : 0;
+    my $strain1 = $input1 =~ /^\d+/? $input1 : 0;
+    my $strain2 = $input2 =~ /^\d+/? $input2 : 0;
     print "./mergeStrains $input1 $strain1 $input2 $strain2 > fifo$fifoCursor &\n";
   }
 }
 print "wait\n";
 print "rm ";
-for (my $i = 1; $i <= scalar(@comparators) *2; $i++) {
+for (my $i = 1; $i <= scalar(@mergeQueueOriginal); $i++) {
   print "fifo$i ";
 }
 print "\n";
